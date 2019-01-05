Correct Data Normalization

util.py
@@ -2,19 +2,27 @@ import numpy as np
 from scipy.misc import imread, imresize, imsave
 import torch
 
+# debug
+# import matplotlib.pyplot as plt
 
 def load_image(filepath):
     image = imread(filepath)
+    # debug
+    # plt.imshow(image)
+    # plt.show()
     if len(image.shape) < 3:
         image = np.expand_dims(image, axis=2)
         image = np.repeat(image, 3, axis=2)
-    image = np.transpose(image, (2, 0, 1))
+    image = np.transpose(image, (2, 0, 1)) # (channels, width, height)
     image = torch.from_numpy(image)
-    min = image.min()
-    max = image.max()
+    # print(image)
     image = torch.FloatTensor(image.size()).copy_(image)
-    image.add_(-min).mul_(1.0 / (max - min))
+    temp_min = image.min()
+    temp_max = image.max()
+    # print("min %f, max %f" % (temp_min, temp_max))
+    image.add_(-temp_min).mul_(1.0 / (temp_max - temp_min))
     image = image.mul_(2).add_(-1)
+    # print(image)
     return image
 
 